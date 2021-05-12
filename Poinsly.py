85871ab4eed2d95607ecc385cdd0de615a89f98a
EdAya-R <79726984+EdAya-R@users.noreply.github.com>
2021-05-11 22:08:59 -0400

    Poinsly.py
    Added try/except for error warnings in quadratic rule
    Added numpy and matplotlib libraries to generate graphs (strange warning appears when graphing quadratics, seems to be something relating to cmath and matplotlib)
    Added the ablility for the quadratic rule to generate a graph with 2 points for if Discriminant is greater than 0 and 1 point if Discriminant is 0
    Added new rule for linear equations that solves for X and generates a graph.

--- Poinsly.py
+++ Poinsly.py
@@ -1,14 +1,15 @@
 from sly import Lexer, Parser
 import cmath
+import numpy as np
+import matplotlib.pyplot as plt
 
 class PoinLexer(Lexer):
-    tokens = { NUMBER, ADD, EXP, TOKEN_X } #,SUB,
+    tokens = { NUMBER, ADD, EXP, TOKEN_X} 
     ignore = ' \t'
 
     # Tokens
-    NUMBER =r'\d+'
+    NUMBER =r'-?\d+'
     ADD = r'\+'
-    #SUB = r'-'
     EXP = r'\^'
     TOKEN_X = r'[xX]'
 
@@ -32,13 +33,11 @@ class PoinParser(Parser):
     precedence = (
         ('left', 'TOKEN_X', 'ADD'),
         ('left', 'EXP'),
-        #('right', 'UMINUS'),
         )
 
     def __init__(self):
         self.names = { }
     
-   #Intermediate Code v
     @_('linear_term')
     def number(self, p):
         return p.linear_term
@@ -56,38 +55,78 @@ class PoinParser(Parser):
         if p.number1 == 2:
             return p.number0
         else:
-            print("Illegal character: ", p.number1)
+            print("Illegal character: X^", p.number1, "Only enter number'2' ")
     
+    @_('linear_term ADD number') 
+    def number(self, p):
+     try:
+        a = (-p.number)/p.linear_term
+        print("Solving for x solution: \n", a)
+        print("Linear equation graph: \n")
+        x = np.linspace(-5, 5, 100)
+        y = p.linear_term * x + p.number
+        plt.title("graph",) 
+        plt.xlabel("x axis") 
+        plt.ylabel("y axis")
+        plt.plot(x,y)
+        plt.grid() 
+        plt.show()
+        return a
+     except:
+          print('Illegal character entered please try again, variable must be x or X')
+
     @_('quadratic_term ADD linear_term ADD number') 
     def number(self, p):
         print('\nQuadratic formula is ax^2 + bx +c = 0 \n' , 'a = ', p.quadratic_term, ', b = ', p.linear_term,
         ', c = ', p.number, '\n')
-        #if p.number1 != 2:
-            #print("Illegal character: ", p.quadratic_term.number1)
-        #else:
-        discriminant = (p.linear_term ** 2) - (4 * p.quadratic_term * p.number)
-        print('Discriminant = ', discriminant)
-        if discriminant > 0:
+        try:
+            a = p.quadratic_term
+            b = p.linear_term
+            c = p.number
+            discriminant = (b ** 2) - (4 * a * c)
+            if discriminant > 0:
                 print('Real and different roots\n')
-                solution1 = (-p.linear_term + cmath.sqrt(discriminant))/(2*p.quadratic_term)
-                solution2 = (-p.linear_term - cmath.sqrt(discriminant))/(2*p.quadratic_term)
-                return solution1 , solution2
-        elif discriminant == 0:
+                solution1 = (-b+ cmath.sqrt(discriminant))/(2*a)
+                solution2 = (-b- cmath.sqrt(discriminant))/(2*a)
+                print(solution1 , solution2)
+                x = np.linspace(-20,20,50)
+                y = a*x**2+b*x+c
+                plt.title("Parabola") 
+                plt.xlabel("x axis") 
+                plt.ylabel("y axis") 
+                plt.plot(x,y)
+                plt.axhline(y=0, color='black', linestyle='-')
+                plt.axvline(x=0, color='black', linestyle='-')
+                plt.plot(solution1, 0, marker="o", color='green')
+                plt.plot(solution2, 0, marker="o", color='green')
+                plt.grid()
+                plt.show()
+                return 
+            elif discriminant == 0:
                 print('Real and same roots\n')
-                solution3 = (-p.linear_term /(2*p.quadratic_term))
-                return solution3
-        else:
+                solution3 = (-b /(2*a))
+                print(solution3)
+                x = np.linspace(-20,20,50)
+                y = a*x**2+b*x+c
+                plt.title("Parabola") 
+                plt.xlabel("x axis") 
+                plt.ylabel("y axis") 
+                plt.plot(x,y)
+                plt.axhline(y=0, color='black', linestyle='-')
+                plt.axvline(x=0, color='black', linestyle='-')
+                plt.plot(solution3, 0, marker="o", color='green')
+                plt.grid()
+                plt.show()
+                return
+            else:
                 print('No real solutions\n')
+                
+        except:
+          print('Error: Illegal character was entered in aX^2, bX or C, Suggest looking at input')
 
-        
-    #Intermediate Code ^
     @_('NUMBER')
     def number(self, p):
         return p.NUMBER
-
-    # @_('SUB number %prec UMINUS')
-    #  def number(self, p):
-    #    return p.number 
 
 if __name__ == '__main__':
     lexer = PoinLexer()
@@ -100,4 +139,3 @@ if __name__ == '__main__':
             print(result)
         except EOFError:
             break
-            
